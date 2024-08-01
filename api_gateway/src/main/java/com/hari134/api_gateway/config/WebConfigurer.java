7b022e62c6330ade2289faac6965920cef1e9fc3
Hari Vishwanath <mailtohari134@gmail.com>
2024-08-01 14:17:07 +0530

    Add static files

---
+++ api_gateway/src/main/java/com/hari134/api_gateway/config/WebConfigurer.java
@@ -0,0 +1,23 @@
+package com.hari134.api_gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // Forward all requests to index.html so that they can be handled by the React router
+        registry.addViewController("/{path:[^\\.]*}")
+                .setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
--- executor/src/main/java/com/hari134/executor/config/RabbitMQConfig.java
+++ executor/src/main/java/com/hari134/executor/config/RabbitMQConfig.java
@@ -5,7 +5,6 @@ import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.Jackson2JavaTypeMapper.TypePrecedence;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
